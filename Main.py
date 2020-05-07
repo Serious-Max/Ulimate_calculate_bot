dd025b0378125f8d6203ae1dc569bc45375f6dc8
qwerty <123@123.com>
2020-05-08 00:45:49 +0500

    Make dialog and auto find proxy

--- Main.py
+++ Main.py
@@ -1,47 +1,53 @@
 from telegram.ext import Updater, MessageHandler, Filters
-from telegram.ext import CallbackContext, CommandHandler
+from telegram.ext import CallbackContext, CommandHandler, ConversationHandler
+from fp.fp import FreeProxy
 import time
 
 
 def start(update, context):
     update.message.reply_text(
         "temp message")
-
+    return 1
 
 def help(update, context):
     update.message.reply_text(
         "this is help")
 
 
-def _time(update, context):
-    update.message.reply_text(str('Текущее время: ' + time.asctime().split()[3]))
-
-
-def date(update, context):
-    temp = time.asctime().split()
-    update.message.reply_text(str(' '.join(["Текущая дата:",
-                                           temp[1],
-                                           temp[2],
-                                           temp[4]])))
-
+def stop(update, context):
+    update.message.reply_text('Stop')
 
 def text(update, context):
-    update.message.reply_text('Я получил сообщение ' + update.message.text)
+    if update.message.text == '/stop':
+        return ConversationHandler.END
+    else:
+        update.message.reply_text('Я получил сообщение ' + update.message.text)
+        return 1
+
 
 
 def main():
-    #request_kwargs = {
-    #    'proxy_url': 'https://200.195.162.242:3128'
-    #}
-    updater = Updater(token='1279678359:AAHt3EhVe3daGWBgyyChxyMgtH1-FgWyQWI', use_context=True)#, request_kwargs=request_kwargs)  # нужен токен бота
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler('time', _time))
-    dp.add_handler(CommandHandler('date', date))
-    dp.add_handler(MessageHandler(Filters.text, text))
-    updater.start_polling()
-    updater.idle()
+    while 1:
+        try:
+            print('start find proxy...')
+            request_kwargs = {
+                'proxy_url': FreeProxy(country_id=['US', 'UK']).get()
+            }
+            print('set proxy', request_kwargs['proxy_url'])
+            updater = Updater(token='1279678359:AAHt3EhVe3daGWBgyyChxyMgtH1-FgWyQWI', use_context=True, request_kwargs=request_kwargs)  # нужен токен бота
+            dp = updater.dispatcher
+            main_handler = ConversationHandler(
+                entry_points=[CommandHandler('start', start)],
+                states = {
+                    1: [MessageHandler(Filters.text, text)]
+                },
+                fallbacks=[CommandHandler('stop', stop)]
+            )
+            dp.add_handler(main_handler)
+            updater.start_polling()
+            updater.idle()
+        except Exception as ex:
+            print('error', ex)
 
 
 if __name__ == '__main__':
